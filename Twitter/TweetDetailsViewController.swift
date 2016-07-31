b0391a9c5fe32b85122f084ee050a0bed4651ab3
Ankit Jasuja <AJasuja@walmartlabs.com>
2016-07-31 03:13:54 -0700

    added empty details view controller

---
+++ Twitter/TweetDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TweetDetailsViewController.swift
+//  Twitter
+//
+//  Created by Ankit Jasuja on 7/31/16.
+//  Copyright © 2016 Ankit Jasuja. All rights reserved.
+//
+
+import Cocoa
+
+class TweetDetailsViewController: UIViewController {
+
+}
