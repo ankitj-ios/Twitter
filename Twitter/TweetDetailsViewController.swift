3a21c4d2463e0de516902dff1d701a611b176fec
Ankit Jasuja <AJasuja@walmartlabs.com>
2016-07-31 03:30:05 -0700

    added detail view controller as placeholder.

--- Twitter/TweetDetailsViewController.swift
+++ Twitter/TweetDetailsViewController.swift
@@ -6,8 +6,16 @@
 //  Copyright © 2016 Ankit Jasuja. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class TweetDetailsViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
+
